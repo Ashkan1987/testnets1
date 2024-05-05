e45ea9af77c6d76bc512504b8e5b49b78899396c
Alpine <94230281+Ashkan1987@users.noreply.github.com>
2024-05-05 09:37:59 +0330

    Update check-gentx-amount.sh

--- scripts/check-gentx-amount.sh
+++ scripts/check-gentx-amount.sh
@@ -25,7 +25,25 @@ if [ "$(jq "$denomquery" "$path")" != "0" ]; then
   echo "invalid denomination"
   exit 1
 fi
+#!/bin/bash
 
+set -eo pipefail
+
+path="$1"
+
+declare -i maxbond=1000000000
+
+extraquery='[.value.msg[]| select(.type != "cosmos-sdk/MsgCreateValidator")]|length'
+
+gentxquery='.value.msg[]| select(.type == "cosmos-sdk/MsgCreateValidator")|.value.value'
+
+denomquery="[$gentxquery | select(.denom != \"star\")] | length"
+
+amountquery="$gentxquery | .amount"
+
+# only allow MsgCreateValidator transactions.
+if [ "$(jq "$extraquery" "$path")" != "0" ]; then
+  echo "spurious transactions"
 # limit the amount that can be bonded
 for amount in "$(jq -rM "$amountquery" "$path")"; do
   declare -i amt="$amount"
